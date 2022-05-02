bdcf69afc7b9b5e15215f5b447573672115064f9
Satit Rakkhaphan <mynung.comsci@gmail.com>
2022-05-03 06:43:42 +0700

    rewrite image

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -8,17 +8,17 @@ const Footer: FunctionComponent = () => {
   return (
     <footer className={styles.footerContainer}>
       <Container className='d-flex align-items-center flex-column pt-5'>
-        <Image src='/images/truemoneywallet-logo.png' alt='true money wallet' width={300} height={88} />
+        <Image src='/images/truemoneywallet-logo.webp' alt='true money wallet' width={300} height={88} />
         <div className='d-flex justify-content-center mt-2' style={{ gap: 12 }}>
-          <Image src='/images/bank_kbank.png' alt='kbank' width={40} height={40} />
-          <Image src='/images/bank_bbl.png' alt='bbl' width={40} height={40} />
-          <Image src='/images/bank_gsb.png' alt='gsb' width={40} height={40} />
-          <Image src='/images/bank_ktb.png' alt='ktb' width={40} height={40} />
-          <Image src='/images/bank_scb.png' alt='scb' width={40} height={40} />
-          <Image src='/images/bank_ttb.png' alt='ttb' width={40} height={40} />
-          <Image src='/images/bank_kiatnakin.png' alt='kiatnakin' width={40} height={40} />
-          <Image src='/images/bank_tisco.png' alt='tisco' width={40} height={40} />
-          <Image src='/images/bank_baac.png' alt='baac' width={40} height={40} />
+          <Image src='/images/bank_kbank.webp' alt='kbank' width={40} height={40} />
+          <Image src='/images/bank_bbl.webp' alt='bbl' width={40} height={40} />
+          <Image src='/images/bank_gsb.webp' alt='gsb' width={40} height={40} />
+          <Image src='/images/bank_ktb.webp' alt='ktb' width={40} height={40} />
+          <Image src='/images/bank_scb.webp' alt='scb' width={40} height={40} />
+          <Image src='/images/bank_ttb.webp' alt='ttb' width={40} height={40} />
+          <Image src='/images/bank_kiatnakin.webp' alt='kiatnakin' width={40} height={40} />
+          <Image src='/images/bank_tisco.webp' alt='tisco' width={40} height={40} />
+          <Image src='/images/bank_baac.webp' alt='baac' width={40} height={40} />
         </div>
         <p className='text-white mt-5'>2020-2021 © Copyright LINWIN168 All rights Reserved.</p>
       </Container>
