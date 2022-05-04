387fc5a8a7a4b16f9cd51c36a010ddca1d77c64d
Satit Rakkhaphan <mynung.comsci@gmail.com>
2022-05-05 01:53:35 +0700

    add banner

--- components/Home/HomeBanner.tsx
+++ components/Home/HomeBanner.tsx
@@ -3,11 +3,14 @@ import Image from 'next/image'
 
 import styles from './HomeBanner.module.scss'
 import classNames from 'classnames'
+import { Container } from 'react-bootstrap'
 
 const HomeHeader: FunctionComponent = () => {
   return (
     <div className={classNames('ratio', styles.bg)}>
-      {/* <Image src='/images/header-banner.jpg' alt='home banner' layout='fill' unoptimized objectFit='contain' /> */}
+      {/* <Container className='ratio'> */}
+      <Image src='/images/home-banner.png' alt='home banner' layout='fill' unoptimized objectFit='contain' />
+      {/* </Container> */}
     </div>
   )
 }
