13b6559b775b8e1b1e1c04323fb2aa3815516e27
Satit Rakkhaphan <mynung.comsci@gmail.com>
2022-05-09 06:26:00 +0700

    add keyword

--- components/Common/Seo.tsx
+++ components/Common/Seo.tsx
@@ -13,28 +13,31 @@ export type SeoProps = {
 }
 
 const Seo: FunctionComponent<SeoProps> = ({ title, description, keywords, shareImageURL, articleInfo }) => {
-  const imageURL = shareImageURL ?? ''
-
   return (
     <Head>
       <title>{title}</title>
       <meta property='og:type' content='website' />
       <meta property='og:locale' content='th_TH' />
-      <meta property='og:site_name' content='lynwin168' />
+      <meta property='og:site_name' content='Lynwin168' />
       <meta name='description' content={description} />
       {keywords && <meta name='keywords' content={keywords} />}
 
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
-      <meta property='og:image' content={imageURL} />
-      <meta property='og:image:secure_url' content={imageURL} />
-      <meta property='og:image:width' content='1200' />
-      <meta property='og:image:height' content='630' />
 
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:title' content={title} />
       <meta name='twitter:description' content={description} />
-      <meta name='twitter:image' content={imageURL} />
+
+      {shareImageURL && (
+        <>
+          <meta property='og:image' content={shareImageURL} />
+          <meta property='og:image:secure_url' content={shareImageURL} />
+          <meta property='og:image:width' content='1200' />
+          <meta property='og:image:height' content='630' />
+          <meta name='twitter:image' content={shareImageURL} />
+        </>
+      )}
 
       {/* Article */}
       {articleInfo && (
@@ -42,7 +45,7 @@ const Seo: FunctionComponent<SeoProps> = ({ title, description, keywords, shareI
           <meta property='og:type' content='article' />
           <meta property='og:updated_time' content={articleInfo.updatedTime} />
           <meta property='article:publisher' content='https://lynwin168.com' />
-          <meta property='article:author' content='lynwin168' />
+          <meta property='article:author' content='Lynwin168' />
           <meta property='article:published_time' content={articleInfo.publishedTime} />
           <meta property='article:modified_time' content={articleInfo.updatedTime} />
         </>
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -12,6 +12,51 @@ interface LayoutProps {
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
   const router = useRouter()
+  const keywords = [
+    'LYN BET',
+    'lynbeer',
+    'lynblue89',
+    'lynwin',
+    'lynwin168',
+    'lyngold',
+    'lynking',
+    '168',
+    'เว็บสล็อต',
+    'สล็อตออนไลน์',
+    'เกมสล็อตออนไลน์ได้เงินจริง',
+    'PGSLOT',
+    'คาสิโน',
+    'คาสิโนออนไลน์',
+    'casino',
+    'บาคาร่า',
+    'เว็บแทงบอลออนไลน์',
+    'ยูฟ่า',
+    'UFA',
+    'พนันออนไลน์',
+    'บริการตลอด 24 ชม',
+    'หวย',
+    'หวยไทย',
+    'หวยลาว',
+    'หวยเวียดนาม',
+    'หวยฮานอย',
+    'หวยมาเล',
+    'หวยสิงคโป',
+    'slot',
+    'slot online',
+    'game slot',
+    'gameslot',
+    'สล็อต',
+    'สล็อตออนไลน์',
+    'เกมสล็อต',
+    'ยิงปลา',
+    'เกมยิงปลา',
+    'joker',
+    'evoplay',
+    'simpleplay',
+    'playstar',
+    'kingmaker',
+    'habanero'
+  ]
 
   return (
     <>
@@ -20,7 +65,7 @@ const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
         <Seo
           title='Lynwin168 ฝาก ถอน ไว มั่นใจ 100%'
           description='lynwin168 ฝาก ถอน ไว มั่นใจ 100%'
-          keywords='LYN BET,lynbeer,lynblue89,lynwin, lynwin168, lyngold,lynking,168,เว็บสล็อต,สล็อตออนไลน์,เกมสล็อตออนไลน์ได้เงินจริง,PGSLOT,คาสิโน,คาสิโนออนไลน์,casino,บาคาร่า,เว็บแทงบอลออนไลน์,ยูฟ่า,UFA,พนันออนไลน์,บริการตลอด 24 ชม,หวย,หวยไทย,หวยลาว,หวยเวียดนาม,หวยฮานอย,หวยมาเล,หวยสิงคโป,slot, slot online, game slot, gameslot, สล็อต, สล็อตออนไลน์, เกมสล็อต, ยิงปลา, เกมยิงปลา, joker, evoplay, simpleplay, playstar, kingmaker, habanero'
+          keywords={keywords.join(', ')}
         />
       )}
       <Header />
