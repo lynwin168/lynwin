1230ebac29abfa71881b1d77399f9a11825f4372
Satit Rakkhaphan <mynung.comsci@gmail.com>
2022-05-08 23:05:01 +0700

    success promotion

--- components/Home/HomeArticle.tsx
+++ components/Home/HomeArticle.tsx
@@ -1,13 +1,21 @@
+import { ArticleAttributesData } from '@/models/article.model'
+import { StrapiDataItem } from '@/models/strapi.model'
 import classNames from 'classnames'
 import Image from 'next/image'
 import Link from 'next/link'
 import { FunctionComponent } from 'react'
 import { Button, Container } from 'react-bootstrap'
 import Slider, { Settings } from 'react-slick'
+import ArticleItem from '../Article/ArticleItem'
+import CardView from '../Common/CardView'
 
 import styles from './HomeArticle.module.scss'
 
-const HomeArticle: FunctionComponent = () => {
+type HomeArticleProps = {
+  articles: StrapiDataItem<ArticleAttributesData>[]
+}
+
+const HomeArticle: FunctionComponent<HomeArticleProps> = ({ articles }) => {
   const settings: Settings = {
     autoplay: true,
     autoplaySpeed: 3000,
@@ -39,11 +47,20 @@ const HomeArticle: FunctionComponent = () => {
 
   return (
     <div className={styles.articleSection}>
-      <Container className='text-center'>
+      <Container>
         <h1 className='text-center text-white'>บทความ</h1>
         <Slider className='home-slick-slider article-slick-slider mt-4' {...settings}>
-          <div className='d-flex justify-content-center'>
-            <Image src='/images/article_01.jpg' alt='article' width={400} height={200} style={{ borderRadius: 5 }} />
+          {articles.map((article) => (
+            <div className='px-2' key={article.id}>
+              <ArticleItem article={article} />
+            </div>
+          ))}
+          {/* <div className='d-flex justify-content-center'>
+            <ArticleItem />
+            <CardView>
+              <Image src='/images/article_01.jpg' alt='article' width={400} height={200} style={{ borderRadius: 5 }} />
+              <div className='p-4'>LUCIABET คาสิโนออนไลน์ ที่ได้ยิ่งใหญ่ในวงการพนันออนไลน์</div>
+            </CardView>
           </div>
           <div className='d-flex justify-content-center'>
             <Image
@@ -64,11 +81,13 @@ const HomeArticle: FunctionComponent = () => {
               height={200}
               style={{ borderRadius: 5 }}
             />
-          </div>
+          </div> */}
         </Slider>
-        <Link href='/article' passHref>
-          <Button className={classNames(styles.viewAllArticle, 'mt-3')}>บทความทั้งหมด</Button>
-        </Link>
+        <div className='text-center'>
+          <Link href='/article' passHref>
+            <Button className={classNames(styles.viewAllArticle, 'mt-3')}>บทความทั้งหมด</Button>
+          </Link>
+        </div>
       </Container>
     </div>
   )
