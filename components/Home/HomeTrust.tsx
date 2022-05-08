8e019522dc6e59f282cd704005c2f2f88b91723a
Satit Rakkhaphan <mynung.comsci@gmail.com>
2022-05-08 23:59:13 +0700

    success trust

--- components/Home/HomeTrust.tsx
+++ components/Home/HomeTrust.tsx
@@ -9,66 +9,60 @@ const HomeTrust: FunctionComponent = () => {
     <section className={styles.trustSection}>
       <Container>
         <Row className='gy-4'>
-          <Col xl={4} md={6}>
+          <Col xl={4} sm={6} xs={6}>
             <HomeTrustItem
               src='/images/ic-trust-smart-wallet.webp'
-              height={115}
-              width={115}
+              imageSize={115}
               alt='smart wallet'
               title='Smart Wallet'
               subTitle='เล่นได้ทุกเกมส์ไม่ต้องโยกเงิน'
               unoptimized
             />
           </Col>
-          <Col xl={4} md={6}>
+          <Col xl={4} sm={6} xs={6}>
             <HomeTrustItem
               src='/images/ic-trust-award.webp'
-              height={115}
-              width={115}
+              imageSize={115}
               alt='number one in thailand'
               title='อันดับ 1 ในไทย'
               subTitle='เทคโนโลยี และเกมส์จากทั่วทุกมุมโลก'
               unoptimized
             />
           </Col>
-          <Col xl={4} md={6}>
+          <Col xl={4} sm={6} xs={6}>
             <HomeTrustItem
               src='/images/ic-trust-exchange.webp'
-              height={115}
-              width={115}
+              imageSize={115}
               alt='exchange'
               title='ฝาก - ถอน เร็วมาก'
               subTitle='ฝาก - ถอน เร็วมากด้วยระบบ Bot AI'
               unoptimized
             />
           </Col>
-          <Col xl={4} md={6}>
+          <Col xl={4} sm={6} xs={6}>
             <HomeTrustItem
               src='/images/ic-trust-game.webp'
-              height={115}
-              width={115}
+              imageSize={115}
               alt='game'
               title='เล่นได้ทุกที'
               subTitle='ระบบใช้งานง่ายรองรับทุกอุปกรณ์'
               unoptimized
             />
           </Col>
-          <Col xl={4} md={6}>
+          <Col xl={4} sm={6} xs={6}>
             <HomeTrustItem
               src='/images/ic-trust-contact.webp'
-              height={115}
-              width={115}
+              imageSize={115}
               alt='contact'
               title='บริการด้วยใจ'
               subTitle='ฝ่ายบริการลูกค้า 24 ชม. ทุกวัน'
               unoptimized
             />
           </Col>
-          <Col xl={4} md={6}>
+          <Col xl={4} sm={6} xs={6}>
             <HomeTrustItem
               src='/images/ic-trust-giff.webp'
-              height={115}
-              width={115}
+              imageSize={115}
               alt='good promotion'
               title='โปรดีๆ สำหรับทุกคน'
               subTitle='กิจกรรมและโปรโมชันหลากหลาย'
--- components/Home/HomeTrustItem.tsx
+++ components/Home/HomeTrustItem.tsx
@@ -6,15 +6,22 @@ import styles from './HomeTrustItem.module.scss'
 type HomeTrustItemProps = ImageProps & {
   title: string
   subTitle: string
+  imageSize: number
 }
 
-const HomeTrustItem: FunctionComponent<HomeTrustItemProps> = ({ title, subTitle, alt, ...imageProps }) => {
+const HomeTrustItem: FunctionComponent<HomeTrustItemProps> = ({ title, subTitle, alt, imageSize, ...imageProps }) => {
+  const mobileImageSize = imageSize / 2
   return (
     <div className={styles.container}>
-      <Image {...imageProps} alt={alt} />
+      <div className='d-block d-sm-none' style={{ minWidth: mobileImageSize }}>
+        <Image width={mobileImageSize} height={mobileImageSize} {...imageProps} alt={alt} />
+      </div>
+      <div className='d-none d-sm-block' style={{ minWidth: imageSize }}>
+        <Image width={imageSize} height={imageSize} {...imageProps} alt={alt} />
+      </div>
       <div className={styles.infoWrapper}>
         <span className={styles.title}>{title}</span>
-        <span className='mt-2'>{subTitle}</span>
+        <span className='mt-2 d-none d-md-block'>{subTitle}</span>
       </div>
     </div>
   )
