291e4ac2ee92fc2c4f7ed45d6bfa7c0852c4539a
lynwin168 <lynwin168@gmail.com>
2022-06-14 21:24:07 +0700

    facebook pixel code

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -84,6 +84,11 @@ const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
                 });
             `}
       </Script>
+      {/* <!-- Facebook Pixel Code --> */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '354541863450733'); fbq('track', 'PageView');`} 
+      </Script>
+      <noscript> <Image height={1} width={1} src={'https://www.facebook.com/tr?id=354541863450733&ev=PageView&noscript=1'} alt="facebook pixel" unoptimized/></noscript>
       {visibleContactButton && (
         <a
           rel="noreferrer"
